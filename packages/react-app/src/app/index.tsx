85dfe70bf4c012201c5ddda33533515d8c105a9f
Skel <leroy.etienne@gmail.com>
2025-06-17 19:38:16 +0200

    Initial commit

---
+++ packages/react-app/src/app/index.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+
+import { theme } from './theme'
+import GlobalStyle from './global-style'
+import Router from './router'
+import './i18n'
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle theme={theme} />
+      <Router />
+    </ThemeProvider>
+  )
+}
